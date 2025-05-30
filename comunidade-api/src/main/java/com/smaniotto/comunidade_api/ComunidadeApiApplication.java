1ae9dd0682ca413e713c655a2f9f3a1c400dea1e
maico-smaniotto <maicosmaniotto@gmail.com>
2025-05-29 23:18:40 -0300

    Busca da previsão do tempo com cache

--- comunidade-api/src/main/java/com/smaniotto/comunidade_api/ComunidadeApiApplication.java
+++ comunidade-api/src/main/java/com/smaniotto/comunidade_api/ComunidadeApiApplication.java
@@ -2,8 +2,10 @@ package com.smaniotto.comunidade_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ComunidadeApiApplication {
 
 	public static void main(String[] args) {
