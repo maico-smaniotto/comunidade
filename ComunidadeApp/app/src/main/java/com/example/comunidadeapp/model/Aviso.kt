2990c9360e3e47e429b1be84f3444ac1a76ac1ec
Maico Smaniotto <maicosmaniotto@gmail.com>
2025-06-14 23:36:08 -0300

    Delete ComunidadeApp directory

--- ComunidadeApp/app/src/main/java/com/example/comunidadeapp/model/Aviso.kt
+++
@@ -1,9 +0,0 @@
-package com.example.comunidadeapp.model
-
-data class Aviso(
-    val id: Long,
-    val cadastro: String,  // String para datas (ISO-8601)
-    val validade: String,
-    val titulo: String,
-    val descricao: String
-)
